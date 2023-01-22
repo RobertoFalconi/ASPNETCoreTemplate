0c190b6bfb5e87ef12e2d46060c4161339185e94
Roberto Falconi <R.Falconi@almaviva.it>
2023-01-22 17:09:30 +0100

    Update to .NET 7

--- ASPNETCoreTemplate/Program.cs
+++ ASPNETCoreTemplate/Program.cs
@@ -10,7 +10,6 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
-
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
