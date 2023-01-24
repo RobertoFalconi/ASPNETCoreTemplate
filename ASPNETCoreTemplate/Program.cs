be2bc317b17cdcc6a47d8bdc3aa54acf1dc1479b
Roberto Falconi <R.Falconi@almaviva.it>
2023-01-24 12:16:39 +0100

    Code cleaning

--- ASPNETCoreTemplate/Program.cs
+++ ASPNETCoreTemplate/Program.cs
@@ -6,44 +6,31 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add loggers to the container.
-builder.Logging.AddJsonConsole();
-
 // Add configurations to the container.
 builder.Configuration
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables();
 
+// Add loggers to the container.
+builder.Logging.AddJsonConsole();
+
+// Parameters
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 // Add services to the container.
-if (builder.Environment.IsDevelopment())
-{
-    builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-}
-else
-{
-    builder.Services.AddRazorPages();
-}
+builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+}
 
 // Add the app to the container.
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-}
-
 app.UseHsts();
 app.UseHttpsRedirection();
 app.UseAuthentication();
@@ -51,5 +38,12 @@ app.UseRouting();
 app.UseAuthorization();
 app.UseStaticFiles();
 app.MapRazorPages();
+app.UseExceptionHandler("/Error");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
 
+// Run the app.
 app.Run();
